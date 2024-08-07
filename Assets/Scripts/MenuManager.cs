d39fba8dc1e2d4bc506f0c3624b30a2a69e3c438
noahhashim1107 <noahhashim1107@gmail.com>
2024-08-07 14:21:57 +0100

    Menu overlay and script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuManager : MonoBehaviour
+{
+    // Public variables to hold references to the different screen panels
+    public GameObject startScreen;
+    public GameObject howToPlayScreen;
+    public GameObject themeScreen;
+
+    // Method called at the start of the game
+    void Start()
+    {
+        // Show the start screen when the game starts
+        ShowStartScreen();
+    }
+
+    // Method to show the start screen
+    public void ShowStartScreen()
+    {
+        // Hide all screens first
+        HideAllScreens();
+        // Then activate the start screen
+        startScreen.SetActive(true);
+    }
+
+    // Method to show the "How To Play" screen
+    public void ShowHowToPlayScreen()
+    {
+        // Hide all screens first
+        HideAllScreens();
+        // Then activate the "How To Play" screen
+        howToPlayScreen.SetActive(true);
+    }
+
+    // Method to show the "Theme" screen
+    public void ShowThemeScreen()
+    {
+        // Hide all screens first
+        HideAllScreens();
+        // Then activate the "Theme" screen
+        themeScreen.SetActive(true);
+    }
+
+    // Method to hide all screens
+    private void HideAllScreens()
+    {
+        // Deactivate the start screen
+        startScreen.SetActive(false);
+        // Deactivate the "How To Play" screen
+        howToPlayScreen.SetActive(false);
+        // Deactivate the "Theme" screen
+        themeScreen.SetActive(false);
+    }
+
+    // Method to load the main game scene
+    public void LoadMainGameScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
